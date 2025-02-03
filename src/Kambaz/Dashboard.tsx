c10f7d7d80a4f33c8d51dd12322e4d76b7717946
AngelaZQ1 <zheng.ang@northeastern.edu>
2025-02-03 15:30:20 -0500

    styling

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Button, Card, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function Dashboard() {
   return (
@@ -5,103 +6,127 @@ export default function Dashboard() {
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
       <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1235/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/nodejs.jpg" width={200} />
-            <div>
-              <h5> CS1235 Node JS </h5>
-              <p className="wd-dashboard-course-title">Node.js</p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1236/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/angular.jpg" width={200} />
-            <div>
-              <h5> CS1236 Angular </h5>
-              <p className="wd-dashboard-course-title">
-                Frontend development with Angular
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1237/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/vuejs.png" width={200} />
-            <div>
-              <h5> CS1237 Vue JS </h5>
-              <p className="wd-dashboard-course-title">
-                Frontend development with Vue.js
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1238/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/python.png" width={200} />
-            <div>
-              <h5> CS1238 Python </h5>
-              <p className="wd-dashboard-course-title">Python</p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1239/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/java.png" width={200} />
-            <div>
-              <h5> CS1239 Java </h5>
-              <p className="wd-dashboard-course-title">Java</p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1240/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/csharp.png" width={200} />
-            <div>
-              <h5> CS1240 C# </h5>
-              <p className="wd-dashboard-course-title">C# </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
+        <Row xs={1} md={5} className="g-4">
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img variant="top" src="/images/reactjs.jpg" width={200} />
+                <Card.Body>
+                  <Card.Title> CS1234 React JS </Card.Title>
+                  <Card.Text className="wd-dashboard-course-title">
+                    Full Stack software developer
+                  </Card.Text>
+                  <Button variant="primary"> Go </Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1235/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img variant="top" src="/images/nodejs.jpg" width={200} />
+                <Card.Body>
+                  <Card.Title> CS1235 Node JS </Card.Title>
+                  <Card.Text className="wd-dashboard-course-title">
+                    Node.js
+                  </Card.Text>
+                  <Button variant="primary"> Go </Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1236/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img variant="top" src="/images/angular.jpg" width={200} />
+                <Card.Body>
+                  <Card.Title> CS1236 Angular </Card.Title>
+                  <Card.Text className="wd-dashboard-course-title">
+                    Frontend development with Angular
+                  </Card.Text>
+                  <Button variant="primary"> Go </Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1237/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img variant="top" src="/images/vuejs.png" width={200} />
+                <Card.Body>
+                  <Card.Title> CS1237 Vue JS </Card.Title>
+                  <Card.Text className="wd-dashboard-course-title">
+                    Frontend development with Vue.js
+                  </Card.Text>
+                  <Button variant="primary"> Go </Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1238/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img variant="top" src="/images/python.png" width={200} />
+                <Card.Body>
+                  <Card.Title> CS1238 Python </Card.Title>
+                  <Card.Text className="wd-dashboard-course-title">
+                    Python
+                  </Card.Text>
+                  <Button variant="primary"> Go </Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1239/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img variant="top" src="/images/java.png" width={200} />
+                <Card.Body>
+                  <Card.Title> CS1239 Java </Card.Title>
+                  <Card.Text className="wd-dashboard-course-title">
+                    Java
+                  </Card.Text>
+                  <Button variant="primary"> Go </Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                to="/Kambaz/Courses/1240/Home"
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img variant="top" src="/images/csharp.png" width={200} />
+                <Card.Body>
+                  <Card.Title> CS1240 C# </Card.Title>
+                  <Card.Text className="wd-dashboard-course-title">
+                    C#{" "}
+                  </Card.Text>
+                  <Button variant="primary"> Go </Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   );
--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -12,10 +12,17 @@ import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
 import ReactIconsSampler from "./ReactIcons";
+import Container from "react-bootstrap/Container";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    <Container>
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>…
       <div id="wd-css-id-selectors">
@@ -65,6 +72,12 @@ export default function Lab2() {
       <GridLayout />
       <Flex />
       <ReactIconsSampler />
-    </div>
+      <BootstrapGrids />
+      <ScreenSizeLabel />
+      <BootstrapTables />
+      <BootstrapLists />
+      <BootstrapForms />
+      <BootstrapNavigation />
+    </Container>
   );
 }
