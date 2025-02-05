896441a728c38d85ec45fd8f0bec459133f70497
AngelaZQ1 <zheng.ang@northeastern.edu>
2025-02-05 16:21:08 -0500

    style edit assignment page

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -1,119 +1,131 @@
+import { Button, Form } from "react-bootstrap";
+
 export default function AssignmentEditor() {
   return (
-    <div id="wd-assignments-editor">
-      <label htmlFor="wd-name">Assignment Name</label>
-      <input id="wd-name" value="A1 - ENV + HTML" />
-      <br />
-      <br />
-      <textarea id="wd-description">
-        The assignment is available online Submit a link to the landing page of
-      </textarea>
-      <br />
-      <table>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-points">Points</label>
-          </td>
-          <td>
-            <input id="wd-points" value={100} />
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-group">Assignment Group</label>
-          </td>
-          <td>
-            <select id="wd-group">
-              <option value="ASSIGNMENTS">ASSIGNMENTS</option>{" "}
+    <>
+      <div
+        id="wd-assignments-editor"
+        className="d-flex flex-column align-items-end w-75"
+      >
+        <Form.Label>Assignment Name</Form.Label>
+        <Form.Control placeholder="A1 - ENV + HTML" />
+        <textarea id="wd-description" className="mt-3">
+          The assignment is available online Submit a link to the landing page
+          of your Web application running on Netlify. The landing page should
+          include the following: - Your full name and section - Links to each of
+          the lab assignments - Link to the Kanbas application - Links to all
+          relevant source code repositories The Kanbas application should
+          include a link to navigate back to the landing page.
+        </textarea>
+        <br />
+        <div className="w-75">
+          <Form.Group
+            controlId="wd-points"
+            className="mt-3 d-flex justify-content-end gap-2 align-items-center"
+          >
+            <Form.Label className="">Points</Form.Label>
+            <Form.Control type="number" className="w-100" />
+          </Form.Group>
+          <Form.Group
+            controlId="wd-group"
+            className="mt-3 d-flex justify-content-end gap-2 align-items-center"
+          >
+            <Form.Label className="me-2">Assignment Group</Form.Label>
+            <Form.Control as="select" className="w-100">
+              <option value="ASSIGNMENTS">ASSIGNMENTS</option>
               <option value="QUIZZES">QUIZZES</option>
-              <option value="EXAMS">EXAMS</option>{" "}
+              <option value="EXAMS">EXAMS</option>
               <option value="PROJECT">PROJECT</option>
-            </select>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-display-grade-as">Display Grade as</label>
-          </td>
-          <td>
-            <select id="wd-display-grade-as">
-              <option value="Percentage">Percentage</option>{" "}
+            </Form.Control>
+          </Form.Group>
+          <Form.Group
+            controlId="wd-display-grade-as"
+            className="mt-3 d-flex justify-content-end gap-2 align-items-center"
+          >
+            <Form.Label className="me-2">Display Grade as</Form.Label>
+            <Form.Control as="select" className="w-100">
+              <option value="Percentage">Percentage</option>
               <option value="Letter">Letter</option>
-            </select>
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-submission-type">Submission Type</label>
-          </td>
-          <td>
-            <select id="wd-submission-type">
-              <option value="Online">Online</option>{" "}
+            </Form.Control>
+          </Form.Group>
+          <Form.Group
+            controlId="wd-submission-type"
+            className="mt-3 d-flex justify-content-end gap-2 align-items-center"
+          >
+            <Form.Label className="me-2">Submission Type</Form.Label>
+            <Form.Control as="select" className="w-100">
+              <option value="Online">Online</option>
               <option value="InPerson">In Person</option>
-            </select>
-          </td>
-        </tr>
-        <tr>
-          <td></td>
-          <td align="right" valign="top">
-            <label>Online Entry Options</label>
-          </td>
-        </tr>
-        <tr>
-          <td></td>
-          <input id="wd-text-entry" type="checkbox"></input>
-          <label htmlFor="wd-text-entry">Text Entry</label>
-        </tr>
-        <tr>
-          <td></td>
-          <input id="wd-website-url" type="checkbox"></input>
-          <label htmlFor="wd-website-url">Website URL</label>
-        </tr>
-        <tr>
-          <td></td>
-          <input id="wd-media-recordings" type="checkbox"></input>
-          <label htmlFor="wd-media-recordings">Media Recordings</label>
-        </tr>
-        <tr>
-          <td></td>
-          <input id="wd-file-upload" type="checkbox"></input>
-          <label htmlFor="wd-file-upload">File Uploads</label>
-        </tr>
-        <tr>
-          <td></td>
-          <td align="left">
-            <label htmlFor="wd-assign-to">Assign to</label>
-          </td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>
-            <input id="wd-assign-to" />
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-due-date">Due</label>
-          </td>
-          <td>
-            <input type="date" id="wd-due-date" />
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-available-from">Available From</label>
-            <input type="date" id="wd-available-from" />
-          </td>
-          <td align="right" valign="top">
-            <label htmlFor="wd-available-to">Until</label>
-            <input type="date" id="wd-available-to" />
-          </td>
-        </tr>
-        <tr>
-          <button>Cancel</button>
-          <button>Save</button>
-        </tr>
-      </table>
-    </div>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group className="mt-3 d-flex flex-column gap-2 align-items-start">
+            <Form.Label className="me-2">
+              <b>Online Entry Options</b>
+            </Form.Label>
+            <div>
+              <Form.Check
+                type="checkbox"
+                id="wd-text-entry"
+                label="Text Entry"
+              />
+              <Form.Check
+                type="checkbox"
+                id="wd-website-url"
+                label="Website URL"
+              />
+              <Form.Check
+                type="checkbox"
+                id="wd-media-recordings"
+                label="Media Recordings"
+              />
+              <Form.Check
+                type="checkbox"
+                id="wd-file-upload"
+                label="File Uploads"
+              />
+            </div>
+          </Form.Group>
+          <Form.Group
+            controlId="wd-assign-to"
+            className="mt-3 d-flex justify-content-end gap-2 align-items-center"
+          >
+            <Form.Label className="me-2">
+              <b>Assign to</b>
+            </Form.Label>
+            <Form.Control type="text" className="w-100" />
+          </Form.Group>
+          <Form.Group
+            controlId="wd-due-date"
+            className="mt-3 d-flex justify-content-end gap-2 align-items-center"
+          >
+            <Form.Label className="me-2">
+              <b>Due</b>
+            </Form.Label>
+            <Form.Control type="date" className="w-100" />
+          </Form.Group>
+          <Form.Group className="mt-3 d-flex justify-content-end gap-2 align-items-center">
+            <div className="me-3">
+              <Form.Label className="me-2">
+                <b>Available From</b>
+              </Form.Label>
+              <Form.Control type="date" />
+            </div>
+            <div>
+              <Form.Label className="me-2">
+                <b>Until</b>
+              </Form.Label>
+              <Form.Control type="date" />
+            </div>
+          </Form.Group>
+        </div>
+
+        <br />
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button className="btn-secondary me-2">Cancel</Button>
+          <Button variant="danger">Save</Button>
+        </div>
+      </div>
+    </>
   );
 }
