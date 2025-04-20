7dda89424165849e6694b3648e464b6f7c366ddc
AngelaZQ1 <zheng.ang@northeastern.edu>
2025-04-20 11:54:11 -0400

    remove comments

--- src/Kambaz/Courses/Quizzes/reducer.ts
+++ src/Kambaz/Courses/Quizzes/reducer.ts
@@ -11,25 +11,6 @@ const quizzesSlice = createSlice({
     setQuizzes: (state, action) => {
       state.quizzes = action.payload;
     },
-    // addModule: (state, { payload: module }) => {
-    //   const newModule: any = {
-    //     _id: uuidv4(),
-    //     lessons: [],
-    //     name: module.name,
-    //     course: module.course,
-    //   };
-    //   state.quizzes = [...state.quizzes, newModule] as any;
-    // },
-    // updateModule: (state, { payload: module }) => {
-    //   state.quizzes = state.quizzes.map((m: any) =>
-    //     m._id === module._id ? module : m
-    //   ) as any;
-    // },
-    // editModule: (state, { payload: moduleId }) => {
-    //   state.quizzes = state.quizzes.map((m: any) =>
-    //     m._id === moduleId ? { ...m, editing: true } : m
-    //   ) as any;
-    // },
   },
 });
 export const { setQuizzes } = quizzesSlice.actions;
